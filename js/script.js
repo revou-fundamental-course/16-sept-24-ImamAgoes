795e6a42ea3b9fedb4f8bc1f43a9239168be3ef7
imam_agoes <imamagoes12@gmail.com>
2024-09-25 20:18:33 +0700

    Validation OK

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 // Meminta User untuk menginput namanya
-const namaUser = prompt("Masukkan nama kamu");
-if (namaUser != null){
-    document.getElementById("nama-user").innerHTML = namaUser + "!";
-}
+// const namaUser = prompt("Insert Your Name");
+// if (namaUser != null){
+//     document.getElementById("nama-user").innerHTML = namaUser + "!";
+// }
 
 // JS untuk sticky header
 window.addEventListener("scroll", function(){
@@ -18,4 +18,33 @@ function changeLogo(){
     } else {
         logoWebsite.src = "assets/logo-agoez.png";
     }
+}
+
+// JS untuk mengambil dan menampilkan form input dari User
+function tampilPesan (){
+    const namaLengkap = document.getElementById("nama-lengkap");
+    const tglLahir = document.getElementById("tgl-lahir");
+    const pesan = document.getElementById("pesan-text");
+    let jenKel;
+    if (document.getElementById("male").checked = true) {
+        jenKel = document.getElementById("male");
+    } else if (document.getElementById("female").checked = true) {
+        jenKel = document.getElementById("female");
+    } else {
+        jenKel = "Jenis Kelamin Belum Dipilih";
+    }
+
+    // Bagian Thank You Message
+    const thankYouSubmit = document.createElement("h1");
+    const isiThankYou = document.createTextNode("Thank You For You Message!");
+    thankYouSubmit.appendChild(isiThankYou);
+    thankYouSubmit.style.color = "#173c80";
+
+    document.getElementById("message-us-2").appendChild(thankYouSubmit);
+
+    // Bagian Message nya
+    const tampilNama = document.createElement("p");
+    tampilNama 
+
+    // console.log (namaLengkap.value + tglLahir.value + pesan.value + jenKel.value);
 }
