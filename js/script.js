73ebdc07798ca2d127709add557033edac9c5ac4
imam_agoes <imamagoes12@gmail.com>
2024-09-25 11:03:39 +0700

    First Stage to Live Website

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 // Meminta User untuk menginput namanya
+
+
+// JS untuk sticky header
+window.addEventListener("scroll", function(){
+    const header = document.querySelector("#header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+window.onscroll = function() { changeLogo() }
+function changeLogo(){
+    const logoWebsite = document.getElementById("logo-website");
+    if (window.scrollY > 0){
+        logoWebsite.src = "assets/logo-agoez-white.png";
+    } else {
+        logoWebsite.src = "assets/logo-agoez.png";
+    }
+}
