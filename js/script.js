c8846f6c235fece71422c883e57d8ae5c535cb0f
imam_agoes <imamagoes12@gmail.com>
2024-09-26 00:15:11 +0700

    Final Finish

--- js/script.js
+++ js/script.js
@@ -22,17 +22,20 @@ function changeLogo(){
 
 // JS untuk mengambil dan menampilkan form input dari User
 function tampilPesan (){
-    const namaLengkap = document.getElementById("nama-lengkap");
-    const tglLahir = document.getElementById("tgl-lahir");
-    const pesan = document.getElementById("pesan-text");
+    const time = new Date ();
+    const namaLengkap = "Your Name : " + document.getElementById("nama-lengkap").value;
+    const tglLahir = "Your Birthday : " + document.getElementById("tgl-lahir").value;
+    
     let jenKel;
     if (document.getElementById("male").checked = true) {
-        jenKel = document.getElementById("male");
+        jenKel = "Your Gender : " + document.getElementById("male").value;
     } else if (document.getElementById("female").checked = true) {
-        jenKel = document.getElementById("female");
+        jenKel = "Your Gender : " + document.getElementById("female").value;
     } else {
         jenKel = "Jenis Kelamin Belum Dipilih";
     }
+
+    const pesan = "Your Message : " + document.getElementById("pesan-text").value;
 
     // Bagian Thank You Message
     const thankYouSubmit = document.createElement("h1");
@@ -40,11 +43,36 @@ function tampilPesan (){
     thankYouSubmit.appendChild(isiThankYou);
     thankYouSubmit.style.color = "#173c80";
 
-    document.getElementById("message-us-2").appendChild(thankYouSubmit);
+    // Bagian untuk waktu sekarang
+    const waktuSubmit = document.createElement("p");
+    const isiWaktuSubmit = document.createTextNode(time);
+    waktuSubmit.appendChild(isiWaktuSubmit);
 
-    // Bagian Message nya
+    // Bagian Message, Nama Lengkap
     const tampilNama = document.createElement("p");
-    tampilNama 
+    const isiTampilNama = document.createTextNode(namaLengkap);
+    tampilNama.appendChild(isiTampilNama);
 
-    // console.log (namaLengkap.value + tglLahir.value + pesan.value + jenKel.value);
+    // Bagian Message, Tanggal Lahir
+    const tampilTgl = document.createElement("p");
+    const isiTampilTgl = document.createTextNode(tglLahir);
+    tampilTgl.appendChild(isiTampilTgl);
+
+    // Bagian Message, Jenis Kelamin
+    const tampilJenKel = document.createElement("p");
+    const isiTampilJenKel = document.createTextNode(jenKel);
+    tampilJenKel.appendChild(isiTampilJenKel);
+
+    // Bagian Message, Isi Text Pesan
+    const tampilTextPesan = document.createElement("p");
+    const isiTampilTextPesan = document.createTextNode(pesan);
+    tampilTextPesan.appendChild(isiTampilTextPesan);
+
+    // Bagian Tampil Hasil Submit Web
+    document.getElementById("message-us-2").appendChild(thankYouSubmit);
+    document.getElementById("message-us-2").appendChild(isiWaktuSubmit);
+    document.getElementById("message-us-2").appendChild(tampilNama);
+    document.getElementById("message-us-2").appendChild(tampilTgl);
+    document.getElementById("message-us-2").appendChild(tampilJenKel);
+    document.getElementById("message-us-2").appendChild(tampilTextPesan);
 }
